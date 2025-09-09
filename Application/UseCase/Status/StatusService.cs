7870700a49c07e8f2a097c5368b32c297f15722a
MiskinichJonathanJ <miskinich.jobs.jonathan@gmail.com>
2025-09-09 19:04:51 -0300

    implementar service de status

---
+++ Application/UseCase/Status/StatusService.cs
@@ -0,0 +1,22 @@
+﻿using Application.DataTransfers.Response;
+using Application.Interfaces.IStatus;
+using Domain.Entities;
+
+namespace Application.UseCase.StatusUse
+{
+    public class StatusService : IStatusService
+    {
+        public readonly IStatusQuery _query;
+        public readonly IStatusMapper _mapper;
+        public StatusService(IStatusQuery query, IStatusMapper mapper)
+        {
+            _query = query;
+            _mapper = mapper;
+        }
+        public async Task<ICollection<GenericResponse>> GetAllStatus()
+        {
+            IEnumerable<Status> statuses = await _query.GetAllStatuses();
+            return statuses.Select(s => _mapper.ToResponse(s)).ToList();
+        }
+    }
+}
