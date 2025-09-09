92f9a56a6a37acae4e16ae6a932556d1d6a4a45b
MiskinichJonathanJ <miskinich.jobs.jonathan@gmail.com>
2025-09-09 19:15:45 -0300

    feat: implementar mapper y  querry  para status

---
+++ Application/Mappers/StatusMapper.cs
@@ -0,0 +1,18 @@
+﻿using Application.DataTransfers.Response;
+using Application.Interfaces.IStatus;
+using Domain.Entities;
+
+namespace Application.Mappers
+{
+    public class StatusMapper : IStatusMapper
+    {
+        public GenericResponse ToResponse(Status status)
+        {
+            return new GenericResponse
+            {
+                id = status.ID,
+                name = status.Name
+            };
+        }
+    }
+}
---
+++ Infrastructure/Querys/StatusQuery.cs
@@ -0,0 +1,21 @@
+﻿using Application.Interfaces.IStatus;
+using Domain.Entities;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Querys
+{
+    public class StatusQuery : IStatusQuery
+    {
+        public readonly AppDbContext _context;
+        public StatusQuery(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<ICollection<Status>> GetAllStatuses()
+        {
+            IQueryable<Status> statuses = _context.Statuses;
+            return await statuses.ToListAsync();
+        }
+    }
+}
