3d73072a2985ae61a79490871403a74eb5fe01e9
MiskinichJonathanJ <miskinich.jobs.jonathan@gmail.com>
2025-09-09 18:53:05 -0300

    implementar service de  categoria

---
+++ Application/UseCase/Category/CategoryService.cs
@@ -0,0 +1,24 @@
+﻿using Application.DataTransfers.Response.Category;
+using Application.Interfaces.ICategory;
+
+namespace Application.UseCase.Category
+{
+    public class CategoryService : ICategoryService
+    {
+        public readonly ICategoryCommand _command;
+        public readonly ICategoryQuery _query;
+        public readonly ICategoryMapper _mapper;
+
+        public  CategoryService(ICategoryCommand command, ICategoryQuery query, ICategoryMapper mapper)
+        {
+            _command = command;
+            _query = query;
+            _mapper = mapper;
+        }
+        public  async Task<ICollection<CategoryResponse>> GetAllCategories()
+        {
+            var categories = await _query.GetAllCategories();
+            return categories.Select(c => _mapper.ToResponse(c)).ToList();
+        }
+    }
+}
