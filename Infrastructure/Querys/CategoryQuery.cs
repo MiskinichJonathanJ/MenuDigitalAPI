9c71785e99a281f044fa520a18ab1d0fbe3b93fb
MiskinichJonathanJ <miskinich.jobs.jonathan@gmail.com>
2025-09-09 18:52:47 -0300

    feat: implementar query para obtener todas las categorias

---
+++ Infrastructure/Querys/CategoryQuery.cs
@@ -0,0 +1,22 @@
+﻿using Application.Interfaces.ICategory;
+using Domain.Entities;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Querys
+{
+    public class CategoryQuery : ICategoryQuery
+    {
+        public readonly AppDbContext _context;
+        public CategoryQuery(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ICollection<Category>> GetAllCategories()
+        {
+            IQueryable<Category> query = _context.Categories;
+            return await query.ToListAsync();
+        }
+    }
+}
