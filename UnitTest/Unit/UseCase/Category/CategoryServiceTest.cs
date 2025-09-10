3410070bd55a68117099fb6ed5f6185da75609af
MiskinichJonathanJ <miskinich.jobs.jonathan@gmail.com>
2025-09-09 22:39:02 -0300

    feat:  agregar test  unitario  para  getall categories

---
+++ UnitTest/Unit/UseCase/Category/CategoryServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using Application.DataTransfers.Response.Category;
+using Domain.Entities;
+using Moq;
+
+namespace UnitTest.Unit.UseCase.CategoryTest
+{
+    public class CategoryServiceTest : CategoryServiceTestBase
+    {
+        [Fact]
+        public async Task GetAllCategories_ShouldReturnMappedCategories()
+        {
+            // ARRANGE
+            var categories = new List<Domain.Entities.Category>
+            {
+                new Category { Id = 1, Name = "Category 1", Description = "test"},
+                new Category { Id = 2, Name = "Category 2", Description = "test"}
+            };
+            mockQuery.Setup(q => q.GetAllCategories())
+                     .ReturnsAsync(categories);
+            mockMapper.Setup(m => m.ToResponse(It.Is<Domain.Entities.Category>(c => c.Id == 1)))
+                      .Returns(new CategoryResponse { Id = 1, Name = "Category 1", Description = "test" });
+            mockMapper.Setup(m => m.ToResponse(It.Is<Domain.Entities.Category>(c => c.Id == 2)))
+                      .Returns(new CategoryResponse {Id = 2, Name = "Category 2", Description = "test" });
+            // ACT
+            var result = await service.GetAllCategories();
+            // ASSERT
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Count);
+            Assert.Contains(result, r => r.Id == 1 && r.Name == "Category 1");
+            Assert.Contains(result, r => r.Id == 2 && r.Name == "Category 2");
+
+            mockQuery.Verify(q => q.GetAllCategories(), Times.Once);
+            mockMapper.Verify(m => m.ToResponse(It.IsAny<Domain.Entities.Category>()), Times.Exactly(2));
+            VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ UnitTest/Unit/UseCase/Category/CategoryServiceTestBase.cs
@@ -0,0 +1,29 @@
+﻿using Application.Interfaces.ICategory;
+using Application.UseCase.Category;
+using Moq;
+
+namespace UnitTest.Unit.UseCase.CategoryTest
+{
+    public class CategoryServiceTestBase
+    {
+        protected Mock<ICategoryQuery> mockQuery = new();
+        protected Mock<ICategoryMapper> mockMapper = new();
+        protected CategoryService service;
+
+        protected CategoryServiceTestBase() 
+        {
+            mockQuery = new Mock<ICategoryQuery>(MockBehavior.Strict);
+            mockMapper = new Mock<ICategoryMapper>(MockBehavior.Strict);
+            service = new CategoryService(
+                mockQuery.Object,
+                mockMapper.Object
+            );
+        }
+
+        protected void VerifyNoOtherCalls()
+        {
+            mockQuery.VerifyNoOtherCalls();
+            mockMapper.VerifyNoOtherCalls();
+        }
+    }
+}
