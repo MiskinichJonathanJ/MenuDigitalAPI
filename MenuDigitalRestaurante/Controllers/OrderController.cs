1bb10fe108f5039e1614a663e15e7f61bc6df274
MiskinichJonathanJ <miskinich.jobs.jonathan@gmail.com>
2025-09-11 20:27:01 -0300

    Implementar endpoints para order

--- MenuDigitalRestaurante/Controllers/OrderController.cs
+++ MenuDigitalRestaurante/Controllers/OrderController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Application.Interfaces.IOrder;
 using Application.DataTransfers.Request.Order;
+using Application.DataTransfers.Request.OrderItem;
 
 namespace MenuDigitalRestaurante.Controllers
 {
@@ -29,6 +30,20 @@ namespace MenuDigitalRestaurante.Controllers
             )
         {
             var result = await _orderService.GetAllOrders(desde, hasta, statusId);
+            return new JsonResult(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrderById([FromRoute] int id)
+        {
+            var result = await _orderService.GetOrderById(id);
+            return new JsonResult(result);
+        }
+
+        [HttpPatch("{id}/Item/{itemId}")]
+        public async Task<IActionResult> UpdateStatusItemOrder([FromRoute] int id, [FromRoute] int itemId, [FromBody] OrderItemUpdateRequest request)
+        {
+            var result = await _orderService.UpdateStatusItemOrder(id, itemId, request);
             return new JsonResult(result);
         }
     }
