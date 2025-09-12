eda5399310644cecf29c6feebbebb7dcc5a90da9
MiskinichJonathanJ <miskinich.jobs.jonathan@gmail.com>
2025-09-12 13:20:11 -0300

    feat: implementar respuestas  de los endpoints

--- MenuDigitalRestaurante/Controllers/OrderController.cs
+++ MenuDigitalRestaurante/Controllers/OrderController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Application.Interfaces.IOrder;
-using Application.DataTransfers.Request.Order;
+﻿using Application.DataTransfers.Request.Order;
 using Application.DataTransfers.Request.OrderItem;
+using Application.DataTransfers.Response.Order;
+using Application.DataTransfers.Response.OrderResponse;
+using Application.Interfaces.IOrder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Management.Compute.Fluent.Models;
 
 namespace MenuDigitalRestaurante.Controllers
 {
@@ -16,6 +19,8 @@ namespace MenuDigitalRestaurante.Controllers
             _orderService = orderService;
         }
         [HttpPost]
+        [ProducesResponseType(typeof(OrderCreateResponse), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ApiError), StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> CreateOrder([FromBody] OrderRequest request)
         {
             var result = await _orderService.CreateOrder(request);
@@ -23,6 +28,8 @@ namespace MenuDigitalRestaurante.Controllers
         }
 
         [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<OrderDetailsResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiError), StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> GetAllOrders(
             [FromQuery] DateTime? desde = null,
             [FromQuery] DateTime? hasta = null,
@@ -34,6 +41,8 @@ namespace MenuDigitalRestaurante.Controllers
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(typeof(OrderDetailsResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiError), StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetOrderById([FromRoute] int id)
         {
             var result = await _orderService.GetOrderById(id);
@@ -41,6 +50,9 @@ namespace MenuDigitalRestaurante.Controllers
         }
 
         [HttpPatch("{id}/Item/{itemId}")]
+        [ProducesResponseType(typeof(OrderUpdateResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiError), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ApiError), StatusCodes.Status404NotFound)]
         public async Task<IActionResult> UpdateStatusItemOrder([FromRoute] int id, [FromRoute] int itemId, [FromBody] OrderItemUpdateRequest request)
         {
             var result = await _orderService.UpdateStatusItemOrder(id, itemId, request);
